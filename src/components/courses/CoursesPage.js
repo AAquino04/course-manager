7a7e180245870717e49532a92efff9719a995428
Anderson Aquino <johndoe@example.com>
2022-03-24 15:12:11 -0300

    refactor add form to courses component

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,40 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: ""
+      }
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  }
+
   render() {
-    return <h2>Courses</h2>
+    const { course } = this.state;
+
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={course.title}
+        />
+
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
