e4d723746d4b986b3210866a49247d0d28244ba0
Anderson Aquino <johndoe@example.com>
2022-03-28 11:01:29 -0300

    add spinner to manageCourse

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
 import CourseForm from "./CourseForm";
+import Spinner from "./../common/Spinner";
 import { newCourse } from "../../../tools/mockData"
 
 function ManageCoursePage({ courses, authors, actions, history, ...props }) {
@@ -44,15 +45,16 @@ function ManageCoursePage({ courses, authors, actions, history, ...props }) {
 
   }
 
-  return (
-    <CourseForm
-      course={course}
-      errors={errors}
-      authors={authors}
-      onChange={handleChange}
-      onSave={handleSave}
-    />
-  );
+  return authors.length === 0 || courses.length === 0 ? (<Spinner />)
+    : (
+      < CourseForm
+        course={course}
+        errors={errors}
+        authors={authors}
+        onChange={handleChange}
+        onSave={handleSave}
+      />
+    );
 }
 
 ManageCoursePage.propTypes = {
